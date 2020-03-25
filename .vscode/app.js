0e3f216d7ee4903909e415837eda0e9c140a2313
Gia Musselwhite <56314402+giorgiamusselwhite@users.noreply.github.com>
2020-03-25 10:20:36 -0400

    day 1 notes

---
+++ .vscode/app.js
@@ -0,0 +1,28 @@
+//red,green,yellow
+let light = "Edis";
+if (light === "green") {
+console.log("Go");
+} else if (light === "Yellow") {
+console.log("Caution!");
+} else {
+console.log("Stop");
+}
+
+
+
+let kristyIntolerant = true;
+let baoBaoIntolerant = false;
+
+if(kristyIntolerant === true &&
+    baoBaoIntolerant === true) {
+    console.log("Get Almond Milk");    
+    } else if(kristyIntolerant === true ||
+        baoBaoIntolerant === true) {
+         console.log("Get Both Milks");  
+        } else {
+         console.log("Just regular milk");   
+        }
+    
+
+
+

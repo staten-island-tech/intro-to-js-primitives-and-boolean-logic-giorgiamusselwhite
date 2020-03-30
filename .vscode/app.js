518b38eb3c780260d6760d65783030a741552714
Gia Musselwhite <56314402+giorgiamusselwhite@users.noreply.github.com>
2020-03-30 10:37:19 -0400

    3/30 notes
    I followed along to the youtube videos

--- .vscode/app.js
+++ .vscode/app.js
@@ -1,4 +1,4 @@
-
+/* 
 console.log ("Connected");
 
 //red,green,yellow
@@ -24,8 +24,96 @@ if(kristyIntolerant === true &&
          console.log("Get Both Milks");  
         } else {
          console.log("Just regular milk");   
-        }
+        } */
     
 
 
+/* function test (argument, argument, argument) {
+
+} */
+
+/* function test() {
+console.log("I am a function");
+}
+test(); */
+/* 
+function argument(argument) {
+console.log(argument);
+}
+ argument("Edis"); 
+function add(x, y) {
+    return x + y; return ends the function
+}
+
+console.log(add(2, 7));
+
+let z = function add(x, y) {
+    return x + y; return ends the function
+};
+
+z(1, 11);
+console.log(z(1, 11)); */
+
+//let x = "x";
+//const z = "z"; 
+
+/* const hello = function () {
+    alert ("hello");
+};
+hello();
+
+const bye = function() {
+    alert("bye");
+    alert("bye");
+    alert("bye");
+    alert("bye");
+    alert("bye");
+    alert("bye");
+    alert("bye");
+};
+
+function beatles {
+    hello();
+    bye();
+    hello();
+    bye();
+}
+beatles(); */
+/* 
+function makeFunc () {
+const name = "Sun";
+console.log(name);
+}
+
+function hello() {
+    const name = "Jaiyu";
+    console.log(name);
+}
+hello();
+makeFunc(); */
+
+/* function makeFunc() {
+
+    const name = "Sun";
+    function displayName () {
+        alert(name);
+    }
+    return displayName;
+}
+
+const myFunc = makeFunc();
+myFunc(); */
+
+/* const names = ["Michelle", "Edis", 
+"Richard", "Vincent", "Gisele"];
+names.push("Katrina");
+const Gisele = names.slice(-1); */
+
+/* const divArr = document.querySelectorAll(".bc");
+function color() {
+    divArr.forEach(div => {
+        div.style.backgroundColor = "blue";
+    });
+}
+color(); */
 
